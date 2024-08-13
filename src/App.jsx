016a47a84b661cb10381e03e65748760bbaf2ead
ashirwad dwivedi <ashirwaddwi5510@gmail.com>
2024-08-13 22:14:57 +0530

    secon commit

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,12 @@ function ncards(val){
 function App(){
 	return(
 	<>
-	<div className="nav">
-		<h1>List of Top 5 Netflix Series</h1>
+	<div className="sm:bg-slate-700 sm:bg-zinc-900 sm:w-full h-[10vh] sm:flex sm:items-center sm:justify-center sm:text-3xl sm:text-white text-2xl font-bold flex items-center justify-center bg-zinc-900 text-white text-center">
+		<h1>List of Top Netflix Series</h1>
 	</div>
+	<div className="cards sm:px-7 sm:pt-5 sm:w-full sm:min-h-screen sm:flex sm:flex-wrap  sm:bg-zinc-800 sm:items-center sm:gap-[2vw] w-full min-h-screen bg-zinc-800 flex flex-wrap px-2 py-4 gap-[4vw] justify-center">
 	{Sdata.map(ncards)}
+	</div>
 	</>
 	)
 }
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 function Card(props){
 	return(
 		<>
-				<div className="card">
-					<img src={props.imgsrc} alt="myPic" className="card__img" />
-					<div className="card__info">
-					<p><span className="card__category">{props.title}</span></p>
-					<h3 className="card__name">{props.name}</h3>
-					<a href={props.links}>
+				<div className="card sm:w-[30vw] sm:min-h-[45vh] bg-zinc-700 text-white rounded-lg sm:px-[2vw] sm:py-3 w-[57vw] h-[49vh] px-1 py-2">
+					<img src={props.imgsrc} alt="myPic" className="card__img sm:w-[100%] sm:h-[20%] object-cover" />
+					<div className="card__info sm:flex sm:flex-col sm:items-center sm:justify-around flex flex-col gap-[2vw] justify-around items-center">
+					<p><span className="card__category sm:text-sm sm:font-medium text-sm font-semibold">{props.title}</span></p>
+					<h3 className="card__name sm:text-3xl sm:font-semibold sm:mb-1 text-2xl font-bold">{props.name}</h3>
+					<a className="bg-blue-500 sm:hover:bg-blue-400 sm:px-3 sm:py-1 rounded-lg px-3 py-[1vw] " href={props.links}>
 						<button> Watch Now!</button>
 					</a>
 					</div>
